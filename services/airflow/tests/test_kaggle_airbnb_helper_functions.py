ce646a513179c74ba0bf433740313a4fbaf350b8
Miguel Santana <msantana09@users.noreply.github.com>
2023-12-16 20:23:16 -0600

    fixing issues in last commit
    forgot to update tests, and include port in trino connection config

--- services/airflow/tests/test_kaggle_airbnb_helper_functions.py
+++ services/airflow/tests/test_kaggle_airbnb_helper_functions.py
@@ -8,7 +8,7 @@ module_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 # Append this directory to sys.path
 sys.path.append(module_dir)
 
-from include.helper_functions.kaggle_airbnb import identify_columns_missing_comments, create_llm_request_batches, build_model_api_payload
+from include.helper_functions.kaggle_airbnb import identify_columns_missing_comments, create_llm_request_batches, build_model_api_payload_csv,list_of_dicts_to_csv
 
 class TestKaggleAirbnb(unittest.TestCase):
     def setUp(self):
@@ -28,10 +28,12 @@ class TestKaggleAirbnb(unittest.TestCase):
 
     def test_build_model_api_payload(self):
         columns = [{'name': 'col1', 'type': 'int'}, {'name': 'col2', 'type': 'string'}]
-        result = build_model_api_payload('context', 'table', columns)
+        result = build_model_api_payload_csv('context', 'table', columns)
+
+
         self.assertEqual(result['context'], 'context')
         self.assertEqual(result['tables'][0]['name'], 'table')
-        self.assertEqual(result['tables'][0]['columns'], columns)
+        self.assertEqual(result['tables'][0]['column_csv'], list_of_dicts_to_csv(columns))
 
 if __name__ == '__main__':
     unittest.main()
